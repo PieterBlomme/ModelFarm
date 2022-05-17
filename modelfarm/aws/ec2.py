8de31d7774cc4e6333a5c19f86fbc80a04e25448
Pieter Blomme <pieter.blomme@robovision.ai>
2022-05-17 17:43:06 +0200

    fetching status of instance

--- modelfarm/aws/ec2.py
+++ modelfarm/aws/ec2.py
@@ -1,15 +1,22 @@
 
+from pickle import NONE
 import boto3
 import logging
-from dataclasses import dataclass
 from typing import Any
+from enum import Enum
 
 logger = logging.getLogger(__name__)
-    
-@dataclass
+
+class InstanceStatus(Enum):
+    NONE = 1
+    INITIALIZING = 2
+    RUNNING = 3
 class EC2SpotInstance:
-    instance_id: str
-    spot_instance_request_id: str
+
+    def __init__(self, client: boto3.client, instance_id: str, spot_instance_request_id: str):
+        self._client = client
+        self.instance_id = instance_id
+        self.spot_instance_request_id = spot_instance_request_id
 
     @classmethod
     def create_spot_instance(self,
@@ -43,7 +50,7 @@ class EC2SpotInstance:
         logger.info(f"Creation response: {response}")
         instance_id = response[0]['InstanceId']
         spot_instance_request_id = response[0]["SpotInstanceRequestId"]
-        return EC2SpotInstance(instance_id=instance_id, spot_instance_request_id=spot_instance_request_id)
+        return EC2SpotInstance(client=client, instance_id=instance_id, spot_instance_request_id=spot_instance_request_id)
 
     @classmethod
     def get_spot_instance(self,
@@ -51,18 +58,31 @@ class EC2SpotInstance:
         spot_instance_request_id: str):
 
         # find existing instances
-        spot_instance_requests = client.describe_spot_instance_requests(Filters=[{'Name':'spot-instance-request-id', 'Values':[spot_instance_request_id]}])["SpotInstanceRequests"]
+        spot_instance_requests = client.describe_spot_instance_requests()["SpotInstanceRequests"]
         logger.info(f"Spot instance requests: {spot_instance_requests}")
         for request in spot_instance_requests:
             if request["SpotInstanceRequestId"] == spot_instance_request_id:
                 instance_id = request['InstanceId']
                 spot_instance_request_id = request["SpotInstanceRequestId"]
-                return EC2SpotInstance(instance_id=instance_id, spot_instance_request_id=spot_instance_request_id)
+                return EC2SpotInstance(client=client, instance_id=instance_id, spot_instance_request_id=spot_instance_request_id)
 
-        raise Exception(f'Instance {spot_instance_request_id} not found')
+        raise Exception(f'Spot request {spot_instance_request_id} not found')
 
-    def terminate(self, client: boto3.client):
-        response = client.cancel_spot_instance_requests(SpotInstanceRequestIds=[self.spot_instance_request_id])
+    def terminate(self):
+        response = self._client.cancel_spot_instance_requests(SpotInstanceRequestIds=[self.spot_instance_request_id])
         logger.info(f"Cancel response: {response}")
-        response = client.terminate_instances(InstanceIds=[self.instance_id])
+        response = self._client.terminate_instances(InstanceIds=[self.instance_id])
         logger.info(f"Termination response: {response}")
+
+    def status(self): 
+        response = self._client.describe_instance_status(InstanceIds=[self.instance_id])
+        if not response['InstanceStatuses']:
+            return InstanceStatus.NONE
+        
+        if response['InstanceStatuses'][0]["InstanceStatus"]["Status"] == "initializing":
+            return InstanceStatus.INITIALIZING
+        else:
+            raise Exception(f"Unexpected instance status: {response}")
+
+    def __str__(self):
+        return f"EC2SpotInstance(instance_id={self.instance_id}, spot_instance_request_id={self.spot_instance_request_id}"
--- testscript.py
+++ testscript.py
@@ -1,7 +1,8 @@
 import logging
 import boto3
+import time
 from pathlib import Path
-from modelfarm.aws.ec2 import EC2SpotInstance
+from modelfarm.aws.ec2 import EC2SpotInstance, InstanceStatus
 from modelfarm.aws.iam import get_or_create_key_pair, get_or_create_security_group
 logging.basicConfig(level="INFO")
 
@@ -16,10 +17,21 @@ key_pair = get_or_create_key_pair(client, Path(f"./.keys/{key_name}"))
 security_group = get_or_create_security_group(client, "ec2-spot-sg")
 
 instance = EC2SpotInstance.create_spot_instance(client, key_name=key_name, security_group=security_group)
-logger.info(f"Spot instance ID: {instance}")
+logger.info(f"Spot instance: {instance}")
 
-instance = EC2SpotInstance.get_spot_instance(client, spot_instance_request_id=instance.spot_instance_request_id)
-logger.info("Spot instance retrieved")
+try:
+    time.sleep(5)
 
-instance.terminate(client)
-logger.info("Spot instance terminated")
+    instance = EC2SpotInstance.get_spot_instance(client, spot_instance_request_id=instance.spot_instance_request_id)
+    logger.info("Spot instance retrieved")
+
+    while True:
+        instance_status = instance.status()
+        logger.info(f"Instance status: {instance_status}")
+        if instance_status == InstanceStatus.RUNNING:
+            logger.info("Instance running")
+            break
+        time.sleep(10)
+finally:
+    instance.terminate()
+    logger.info("Spot instance terminated")
